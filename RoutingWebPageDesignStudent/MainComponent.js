e67faa3a840cc85ecc9a820fd2844b542aa35702
Jayesh Shinde <35921023+JayeshMShinde@users.noreply.github.com>
2023-03-20 11:16:55 +0530

    Update MainComponent.js

--- RoutingWebPageDesignStudent/MainComponent.js
+++ RoutingWebPageDesignStudent/MainComponent.js
@@ -1,3 +1,8 @@
+/**
+ * It renders a navbar, a sidebar, and a main content area
+ * @returns A React component.
+ */
+
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { DashboardCom } from "./DashboardComponent";
@@ -11,28 +16,42 @@ import { RoleCom } from "./RoleComponent";
 import { TopicCom } from "./TopicComponent";
 
 export const MainComWebDes = () => {
-    const [divstyle] = useState({ "min-height": "500px", "width": "100%" });
+    const [divstyle] = useState({ "min-height": "500px", "width": "90%" });
     const [divheightStyle] = useState({ "height": "100%" });
     const [divwidthStyle] = useState({ "width": "100%" });
+    const [divinputserch] = useState({ "height": "100%", "width": "250px" })
     const [divcolor] = useState({ "color": "white" });
-    const [linkcol] = useState({ "color": "#00A0C6", "text-decoration": "none", "cursor": "pointer" ,"width":"100%"});
-    const [stylefooter] = useState({ "background-color": "rgba(0, 0, 0, 0.2)", "width": "100%" });
+    const [fontsize] = useState({ "font-size": "25px" });
+    const [linkcol] = useState({ "color": "#00A0C6", "text-decoration": "none", "cursor": "pointer", "width": "100%"});
+    const [fontstyle] = useState({ "font-family": "cursive"})
     return (
-        <div>
+        <div style={fontstyle}>
             <Router>
                 {/* Header Section  */}
                 <div>
                     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                         <div className="container-fluid">
-                            <a href="#" className="navbar-brand">CIIT</a>
+                            <a href="http://localhost:3000" className="navbar-brand" style={divheightStyle}>
+                                &nbsp;
+                                <b>CIIT</b>
+                            </a>
                             <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse1">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
+
                             <div className="collapse navbar-collapse" id="navbarCollapse1">
                                 <div className="navbar-nav ms-auto">
-                                    <a href="#Login" className="nav-item nav-link"><i className="bi bi-person-circle">&nbsp;</i>Login</a>
+                                    <div className="input-group" style={divinputserch}>
+                                        <input type="text" className="form-control" placeholder="Search" />
+                                        <button type="button" className="btn btn-primary"><i className="bi-search"></i></button>
+                                    </div>
+                                    &nbsp;
+                                    <a href="#Register" className="nav-item nav-link"><i className="bi-person-plus-fill">&nbsp;</i>Sign Up</a>
+                                    &nbsp;
+                                    <a href="#Login" className="nav-item nav-link"><i className="bi bi-person-fill">&nbsp;</i>Sign In</a>
                                 </div>
                             </div>
+
                         </div>
                     </nav>
                 </div>
@@ -41,55 +60,55 @@ export const MainComWebDes = () => {
                     <div className="row">
                         <div className="col-md-3">
                             <div className=" container bg-dark" style={divheightStyle}>
-                                <hr/>
-                                <p style={divcolor}>
-                                    
-                                    Student Section
-                                    <hr />
-                                </p>
-                                <ul className="list-group">
-                                    <li className="list-group-item">
-                                        <Link to="/dashboard" className="btn btn-primary text-light" style={linkcol}><i className="bi bi-house-door-fill"></i>&nbsp;Dashboard</Link>
-                                    </li>
-                                    <li className="list-group-item">
-                                        <Link to="/all-student" className="btn btn-dark text-light" style={linkcol}><i className="bi bi-person-fill"></i>&nbsp;All Student</Link>
-                                    </li>
-                                    <li className="list-group-item">
-                                        <Link to="/all-exam" className="btn btn-dark text-light" style={linkcol}><i className="bi bi-clipboard2-fill"></i>&nbsp; All Exam</Link>
-                                    </li>
-                                </ul>
-                                <p style={divcolor}>
-                                    <hr />
-                                    Master Section
-                                    <hr />
-                                </p>
-                                <ul className="list-group">
-                                    <li className="list-group-item">
-                                        <Link to="/topic" className="btn btn-dark text-light" style={linkcol}>
-                                            <i className="bi bi-textarea-t"></i>&nbsp;Topic</Link></li>
-                                    <li className="list-group-item">
-                                        <Link to="/content" className="btn btn-dark text-light" style={linkcol}>
-                                            <i className="bi bi-text-left"></i>&nbsp;Content</Link></li>
-                                    <li className="list-group-item">
-                                        <Link to="/content-question" className="btn btn-dark text-light" style={linkcol}>
-                                        <i className="bi bi-file-earmark-text-fill"></i>&nbsp;Content Question</Link></li>
-                                    <li className="list-group-item">
-                                        <Link to="/interview-question" className="btn btn-dark text-light" style={linkcol}>
-                                        <i className="bi bi-journal-richtext"></i>&nbsp;Interview Question</Link></li>
-                                    <li className="list-group-item">
-                                        <Link to="/question-level" className="btn btn-dark text-light" style={linkcol}>
-                                        <i className="bi bi-journal-text"></i>&nbsp;Question Level</Link></li>
-                                    <li className="list-group-item">
-                                        <Link to="/role" className="btn btn-success text-light" style={linkcol}>
-                                        <i className="bi bi-people-fill"></i>&nbsp;Role</Link></li>
-                                </ul>
-                                <p>
-                                    &nbsp;
 
+                                <p style={divcolor}>
+                                    &nbsp;
+                                    <hr />
+                                    <b style={fontsize}>Student Section</b>
+                                    <hr />
+                                </p>
+                                <ul className="list-group">
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/dashboard" className="btn btn-primary text-light rounded-pill" style={linkcol}><i className="bi bi-house-door-fill"></i>&nbsp;<b>Dashboard</b></Link>
+                                    </li>
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/all-student" className="btn btn-danger text-light rounded-pill" style={linkcol}><i className="bi bi-person-fill"></i>&nbsp;<b>All Student</b></Link>
+                                    </li>
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/all-exam" className="btn btn-danger text-light rounded-pill" style={linkcol}><i className="bi bi-clipboard2-fill"></i>&nbsp;<b> All Exam</b></Link>
+                                    </li>
+                                </ul>
+                                <p style={divcolor}>
+                                    <hr />
+                                    <b style={fontsize}>Master Section</b>
+                                    <hr />
+                                </p>
+                                <ul className="list-group">
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/topic" className="btn btn-warning text-dark rounded-pill" style={linkcol}>
+                                            <i className="bi bi-textarea-t"></i>&nbsp;<b>Topic</b></Link></li>
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/content" className="btn btn-warning text-dark rounded-pill" style={linkcol}>
+                                            <i className="bi bi-text-left"></i>&nbsp;<b>Content</b></Link></li>
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/content-question" className="btn btn-warning text-dark rounded-pill" style={linkcol}>
+                                            <i className="bi bi-file-earmark-text-fill"></i>&nbsp;<b>Content Question</b></Link></li>
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/interview-question" className="btn btn-warning text-dark rounded-pill" style={linkcol}>
+                                            <i className="bi bi-journal-richtext"></i>&nbsp;<b>Interview Question</b></Link></li>
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/question-level" className="btn btn-warning text-dark rounded-pill" style={linkcol}>
+                                            <i className="bi bi-journal-text"></i>&nbsp;<b>Question Level</b></Link></li>
+                                    <li className="list-group-item bg-dark">
+                                        <Link to="/role" className="btn btn-info text-dark rounded-pill" style={linkcol}>
+                                            <i className="bi bi-people-fill"></i>&nbsp;<b>Role</b></Link></li>
+                                </ul>
+                                <p style={divcolor}>
+                                    <hr/>
                                 </p>
                             </div>
                         </div>
-                        <div className="col-md-9">
+                        <div className="col-md-8">
                             <Routes>
                                 {/* student */}
                                 <Route path="dashboard" element={<DashboardCom />} />
@@ -107,36 +126,32 @@ export const MainComWebDes = () => {
 
                     </div>
                 </div>
-                <p style={divcolor}>
-                    <hr />
-                </p>
                 {/* Footer Section */}
                 <div className="col-md-12" style={divwidthStyle}>
                     <div style={divheightStyle}>
                         <footer className="bg-dark text-center text-lg-start">
                             <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-                                <div className="me-5 d-none d-lg-block text-light">
-                                    <span>Get connected with us on social networks:</span>
-                                </div>
+                                
                                 <div>
-                                    <a href="https://facebook.com" className="me-4 link-secondary">
+                                    <a href="https://facebook.com" className="me-5 link-light">
                                         <i className="bi bi-facebook"></i>
                                     </a>
-                                    <a href="https://twitter.com" className="me-4 link-secondary">
+                                    <a href="https://twitter.com" className="me-5 link-light">
                                         <i className="bi bi-twitter"></i>
                                     </a>
-                                    <a href="https://google.com" className="me-4 link-secondary">
+                                    <a href="https://google.com" className="me-5 link-light">
                                         <i className="bi bi-google"></i>
                                     </a>
-                                    <a href="https://instagram.com" className="me-4 link-secondary">
+                                    <a href="https://instagram.com" className="me-5 link-light">
                                         <i className="bi bi-instagram"></i>
                                     </a>
                                 </div>
-                            
-                            <div className="text-center p-3 text-light" style={stylefooter}>
-                                ©2023 Copyright: &nbsp;
-                                <a class="text-light" href="https://closetoparkarati.blogspot.com/">Close To Parkarati</a>
-                            </div>
+                                <div className="text-center p-2 text-light" >
+                                    ©2023 Copyright:    
+                                    <a class="text-light" href="https://github.com/JayeshMShinde"><i>JayeshShidne</i></a>
+                                    &nbsp;
+                                    All Rigth Resevered ❤
+                                </div>
                             </section>
                         </footer>
                     </div>
